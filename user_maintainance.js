c1ed67a1f90d0253777afd7b918b0142b13b9ce7
arbeltzoran <arbel.tzoran.98@gmail.com>
2023-09-26 18:18:07 +0300

    popup and menu in maitainance

--- user_maintainance.js
+++ user_maintainance.js
@@ -5,12 +5,19 @@ const jsonData = [
     { "name": "benni", "last_name": "bann", "mail": "n1baarez@gmail.com", "d_o_b": "21/04/1992", "picture":"man3.jpg" }
 ];
 
-function generateGridRows(data) {
+const jsonData2 = [
+    { "name": "arbel", "last_name": "yahoo", "mail": "lucyhaim@gmail.com", "d_o_b": "28/09/1999", "picture":"man1.jpg", "when":"1998", "who":"vered","did":"no" },
+    { "name": "reut", "last_name": "benaiun", "mail": "balazor@gmail.com", "d_o_b": "23/04/1999","picture":"man2.jpg", "when":"1999", "who":"shua","did":"no!" },
+    { "name": "daniel", "last_name": "bann", "mail": "n1baarez@gmail.com", "d_o_b": "21/04/1999", "picture":"man3.jpg", "when":"התשב", "who":"glazer","did":"maybe" }
+];
+
+function generateGridRows(users1, users2) {
     //table item access
     const gridContainer = $("#grid");
+    const gridContainer2 = $("#grid2");
 
     //for each object from the db
-    data.forEach(item => {
+    users1.forEach(item => {
         //create a bootstrap row with an image, name, last name, mail, date of birth and a delete button
         const rowHtml = `
         <div class="row">
@@ -31,8 +38,60 @@ function generateGridRows(data) {
         //add the row to the table item
         gridContainer.append(rowHtml);
     });
+
+    var i = 0;
+    //for each object from the db
+    users2.forEach(item => {
+        //create a bootstrap row with an image, name, last name, mail, date of birth and buttons for accepting, denying and
+        //showing the quiz answers
+        const rowHtml = `
+        <div class="row">
+            <div class="col-2 text-center fs-5 my-2">
+                    <div class="pics">
+                    <img src="${item.picture}">
+                </div>
+            </div>
+            <div class="col-1 text-center my-2">${item.name}</div>
+            <div class="col-1 text-center my-2">${item.last_name}</div>
+            <div class="col-2 text-center my-2 mail">${item.mail}</div>
+            <div class="col-1 text-center my-2 date">${item.d_o_b}</div>
+            <div class="col-1 plus">
+                <img src="plus.jpg" id="plus${i}">
+            </div>
+            <div class="col-1 d-flex align-items-center accept">
+                <button class="crud"><b>אשר</b></button>
+            </div>
+            <div class="col-1 d-flex align-items-center deny">
+                <button class="crud"><b>דחה</b></button>
+            </div>
+        </div>
+        `;
+        i++;
+        //add the row to the table item
+        gridContainer2.append(rowHtml);
+    });
     //add a border outside the table
     gridContainer.addClass("bord");
+    gridContainer2.addClass("bord");
+    return i;
 }
+var countrows;
+$(document).ready(function() {
+    countrows = generateGridRows(jsonData, jsonData2);
+    for (let index = 0; index < countrows; index++) {
+        $('#plus' + index).click(function() {
+            const dataIndex = index;
+            $('.popup').css('display', 'flex');
+            $('#love').val(jsonData2[dataIndex]["when"]);
+            $('#player').val(jsonData2[dataIndex]["who"]);
+            $('#red').val(jsonData2[dataIndex]["did"]);
+        });
+    }
+    
+});
+const gridContainer = $("#grid");
+const gridContainer2 = $("#grid2");
 
-generateGridRows(jsonData);
+$('.close').click(function() {
+    $('.popup').css('display', 'none');
+});
