b17b389ccd91dd1e3d93ac50f744a5186188baa5
TopazK <topaz37@walla.com>
2023-09-30 22:10:00 +0300

    MVC

--- Models/Users.js
+++ Models/Users.js
@@ -5,19 +5,24 @@ const Schema = mongoose.Schema;
 const User = new Schema({
     Email : {
         type: String,
-        required: true
+        required: [true, 'User email required'],
+        validate: {
+            validator: () => Promise.resolve(false),
+            message: 'Email validation failed.'
+          }
     },
     Password: {
         type: String,
-        required: true
+        required: true,
+        min: [4,'Password must contain 4 characters']
     },
     First_Name : {
         type: String,
-        required: true
+        required: [true, 'User first name required']
     },
     Last_Name : {
         type: String,
-        required: true
+        required: [true,  'User last name required']
     },
     Date_Of_Birth : {
         type: Date,
@@ -29,15 +34,15 @@ const User = new Schema({
     },
     When : {
         type: String,
-        required: true
+        required: [true, 'You must answer all the following question.']
     },
     Who : {
         type: String,
-        required: true
+        required: [true, 'You must answer all the following question.']
     },
     Did : {
         type: String,
-        required: true
+        required: [true, 'You must answer all the following question.']
     }
 });
 
