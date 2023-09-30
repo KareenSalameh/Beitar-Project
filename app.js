0ae39681e2de23d54d4996468e465e7437771c5a
TopazK <topaz37@walla.com>
2023-09-30 23:45:51 +0300

    MVC

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const users = require('./routes/Users');
 
-require('custom-env').env(process.env.NODE_ENV, './config');
+require('custom-env').env(process.env.NODE_ENV, './Config');
+
+// app.use(express.static('View'))
 
 mongoose.connect(process.env.CONNECTION_STRING, 
                 {   useNewUrlParser: true, 
@@ -15,6 +17,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.json());
 
-app.use('/users', users);
+app.use('/', users);
 
 app.listen(process.env.PORT);
