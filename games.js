830194ed347e2e558b429d9af50b51669e0c4796
arbeltzoran <arbel.tzoran.98@gmail.com>
2023-10-03 18:06:32 +0300

    games popup

--- games.js
+++ games.js
@@ -1,24 +1,15 @@
 
-var countrows;
+
 //once page was uploaded
 $(document).ready(function() {
     //add rows to table and return their number
-    countrows = generateGridRows(jsonData);
+    const stadiums = $('.stadium');
 
     //for each row added
-    for (let index = 0; index < countrows; index++) {
-        //once clicking th plus image
-        $('#plus' + index).click(function() {
-            //moving the counter to different variable for preventing ambguity
-            const dataIndex = index;
-
-            $('.popup').css('display', 'flex');
-            $('#love').val(jsonData2[dataIndex]["when"]);
-            $('#player').val(jsonData2[dataIndex]["who"]);
-            $('#red').val(jsonData2[dataIndex]["did"]);
-        });
-    }
-    
+    stadiums.click(function() {
+        $('.popup').css('display', 'flex');
+        $('#popup_title').text($(this).text().trim());
+    });    
 });
 
 $('.close').click(function() {
