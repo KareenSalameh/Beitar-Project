ea79d7b4fa82fa2bd92fe8164c4e6cb0826682ce
arbeltzoran <arbel.tzoran.98@gmail.com>
2023-10-14 20:55:14 +0300

    gitignore

--- register.js
+++
@@ -1,8 +0,0 @@
-//when clicking the quiz button the popup will be shown
-//when clicking the close it will disappear
-document.getElementById("quiz").addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "flex";
-});
-document.querySelector(".close").addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "none";
-});
